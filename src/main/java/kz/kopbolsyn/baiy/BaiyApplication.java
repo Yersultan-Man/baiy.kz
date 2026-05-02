62676fd11868f6323243cce7fa65e9bfffd12852
Yersultan-Man <yersultan.kopbolsyn@narxoz.kz>
2026-05-02 15:38:21 +0500

    first commit

---
+++ src/main/java/kz/kopbolsyn/baiy/BaiyApplication.java
@@ -0,0 +1,13 @@
+package kz.kopbolsyn.baiy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaiyApplication.class, args);
+    }
+
+}
